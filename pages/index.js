4ec96c55296b251d8ff964f1f69b4308c8b923f0
Stephen Warren <46313209+jayareX7@users.noreply.github.com>
2021-01-18 14:14:50 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import Head from 'next/head';
+import Intro from '../components/intro';
+import { CMS_NAME } from '../lib/constants';
+import Container from '../components/container'
+import IndexHero from '../components/home-header';
+import Stservice from '../components/home-features';
+import Erpanalytics from '../components/home-analytics';
+import Footer from '../components/Footer';
 
 
 
@@ -10,7 +17,29 @@ export default function Index() {
   return (
 
   <>
- 
+ <IndexHero></IndexHero>
+         
+
+
+<Erpanalytics />
+<Intro />
+<div className="flex-col  flex items-center md:justify-between mt-16 mb-16 md:mb-12"> <a href="/#" className="er_btn video-btn er_btn_two">Get Started Today!</a> 
+            <style jsx>{" .video-btn { position: relative;} {z-index:999;}"}
+                </style></div>
+             
+<Stservice />
+
+
+
+        
+        <section >
+                <video autoPlay loop muted src="assets/home1/AnaRose-Hero.mp4" className= "video-header" alt="">
+                   <style jsx>{" .video-header { position: absolute; width: -webkit-fill-available; padding-bottom: 2rem; z-index: -9999}    {top: 0;}"}
+                </style>
+                </video>  
+             
+            </section>
+        
       
     </>
    
