2142e24a8148fd7819807799889ae5cb3283b709
Stephen Warren <46313209+jayareX7@users.noreply.github.com>
2021-02-13 09:15:59 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,8 @@ import { CMS_NAME } from '../lib/constants';
 import Container from '../components/container'
 import IndexHero from '../components/home-header';
 import Erpanalytics from '../components/home-analytics';
-
+import Featuresitems from '../components/features-items';
+import Footer from '../components/Footer';
 
 
 
@@ -29,21 +30,54 @@ export default function Index() {
              
 
 
-
-
         
         <section >
-                <video autoPlay loop muted src="assets/home1/AnaRose-Hero.mp4" className= "video-header" alt="">
-                   <style jsx>{" .video-header { position: absolute; width: -webkit-fill-available; padding-bottom: 2rem; z-index: -9999}    {top: 0;}"}
-                </style>
-                </video>  
+        <div className = "video-header">
+            <img src="assets/home-banner.png" alt=""/>
+       
+            </div>
+               
              
             </section>
-        
-      
-    </>
-   
+
+
+
+<Container>
+  
+<section className="process_area sec_pad">
+                <div className="container">
+                    <div className="features_info">
+                        <img className="dot_img" src='assets/home4/divider.png' alt=""/>
+                        <Featuresitems rowClass="row flex-row-reverse" aClass="pr_70 pl_70" fimage="process_1.png" iImg="icon01.png" ftitle="Get Started" descriptions="Why I say old chap that is spiffing bodge, blag pardon me buggered mufty Oxford butty bubble and squeak wind up, brown bread the full monty bloke ruddy cras tickety-boo squiffy. Starkers dropped a clanger lurgy is cack excuse my French what a plonker blower.!"/>
+                        <Featuresitems rowClass="row" aClass="pl_100" fimage="process_2.png" iImg="icon02.png" ftitle="Choose A Service"  descriptions="Why I say old chap that is spiffing bodge, blag pardon me buggered mufty Oxford butty bubble and squeak wind up, brown bread the full monty bloke ruddy cras tickety-boo squiffy. Starkers dropped a clanger lurgy is cack excuse my French what a plonker blower.!"/>
+                        <Featuresitems rowClass="row flex-row-reverse" aClass="pr_70 pl_70" fimage="process_3.png" iImg="icon3.png" ftitle="Choose Add-Ons"
+                        descriptions="Why I say old chap that is spiffing bodge, blag pardon me buggered mufty Oxford butty bubble and squeak wind up, brown bread the full monty bloke ruddy cras tickety-boo squiffy. Starkers dropped a clanger lurgy is cack excuse my French what a plonker blower.!"/>
+                        <Featuresitems rowClass="row" aClass="pl_100" fimage="process_4.png" iImg="icon_04.png" ftitle="Confirm Booking" 
+                        descriptions="Why I say old chap that is spiffing bodge, blag pardon me buggered mufty Oxford butty bubble and squeak wind up, brown bread the full monty bloke ruddy cras tickety-boo squiffy. Starkers dropped a clanger lurgy is cack excuse my French what a plonker blower.!"/>
+                        <Featuresitems rowClass="row flex-row-reverse" aClass="pr_70 pl_70" fimage="process_5.png" iImg="icon_05.png" ftitle="Confirm Booking" 
+                        descriptions="Why I say old chap that is spiffing bodge, blag pardon me buggered mufty Oxford butty bubble and squeak wind up, brown bread the full monty bloke ruddy cras tickety-boo squiffy. Starkers dropped a clanger lurgy is cack excuse my French what a plonker blower.!"/>
+                        <div className="dot middle_dot"><span className="dot1"></span><span className="dot2"></span></div>
+                    </div>
+                </div>
+                                
+                           
+            </section>
+
+ <div className=" seo_banner_content col-md-12 text-center">
+                            <h2>Ready to get started?<br/> lt's fast, free and very easy!</h2>
+                        </div>
+                            <div className='text-center'>
+                            <a href="/" className="about_btn">Get A Free Estimate</a>
+                            </div>
+                    
+            
+           
+</Container>
+
+ 
+                </>
 );
+
 
 
 }
