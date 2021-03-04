95e115f3858e390a774f349ade3e86236924f6d7
Admin <jayarex07@gmail.com>
2021-03-04 12:24:18 -0500

    update

--- pages/reviews.js
+++ pages/reviews.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import EventDetails from '../components/event-details';
-import SectionCarousel from '../components/testimonials'
-import FreeEstimate from '../components/free-estimate'
-import ReviewCTA from '../components/review-cta'
-import Footer2 from '../components/footer-2'
-import Portfolio3grid from '../components/portfolios/Portfolio3grid'
-import Portfoliofull4col from '../components/portfolios/Portfolio3grid'
-import Eventgallery from '../components/portfolios/Eventgallery'
+import SectionCarousel from '../components/testimonials';
+import FreeEstimate from '../components/free-estimate';
+import ReviewCTA from '../components/review-cta';
+import Footer2 from '../components/footer-2';
+import Eventgallery from '../components/portfolios/Eventgallery';
 import ReviewStats from '../components/review-stats';
 
 
