bc3e51cc00ab7433c68e873a92f4a51e5506ad37
Admin <jayarex07@gmail.com>
2021-04-02 21:57:28 -0400

    Major Changes

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import Index4 from '../components/index-4'
 import Index5 from '../components/index-5'
 import Footer2 from '../components/footer-2'
 import FreeEstimate from '../components/free-estimate';
+import Navigation from '../components/navigation.js';
+import NavigationTop from '../components/navigationTop'
 
 
 
@@ -14,7 +16,9 @@ export default function Index() {
   return (
 
   <>
-
+<NavigationTop /> 
+<img src="assets/video_bg.jpg" style={{ margin: ' auto 0'}} />
+<Navigation />
          
 
 
@@ -24,32 +28,56 @@ export default function Index() {
                 <div className="container">
                     <div className="features_info">
                         <img className="dot_img" src='assets/home4/divider.png' alt=""/>
-                        <Featuresitems rowClass="row flex-row-reverse" aClass="pr_70 pl_70" fimage="process_1.png" iImg="icon01.png" ftitle="Get Started" descriptions="Why I say old chap that is spiffing bodge, blag pardon me buggered mufty Oxford butty bubble and squeak wind up, brown bread the full monty bloke ruddy cras tickety-boo squiffy. Starkers dropped a clanger lurgy is cack excuse my French what a plonker blower.!"/>
-                        <Featuresitems rowClass="row" aClass="pl_100" fimage="process_2.png" iImg="icon02.png" ftitle="Choose A Service"  descriptions="Why I say old chap that is spiffing bodge, blag pardon me buggered mufty Oxford butty bubble and squeak wind up, brown bread the full monty bloke ruddy cras tickety-boo squiffy. Starkers dropped a clanger lurgy is cack excuse my French what a plonker blower.!"/>
-                        <Featuresitems rowClass="row flex-row-reverse" aClass="pr_70 pl_70" fimage="process_3.png" iImg="icon3.png" ftitle="Choose Add-Ons"
+
+                        <Featuresitems rowClass="row flex-row-reverse" aClass="pr_70 pl_70" fimage="get-started.jpg" iImg="icon01.png" ftitle="Get Started"
+                        descriptions="Why I say old chap that is spiffing bodge, blag pardon me buggered mufty Oxford butty bubble and squeak wind up, brown bread the full monty bloke ruddy cras tickety-boo squiffy. Starkers dropped a clanger lurgy is cack excuse my French what a plonker blower.!" />
+                        <div className="agency_featured_content wow fadeInLeft" data-wow-delay="0.6s">
+                            <a href="/register"><button className="snipcart-register icon mt_30"><i className="ti-arrow-right"></i></button></a> 
+                        </div>
+
+                        <Featuresitems rowClass="row" aClass="pl_100" fimage="choose-service.jpg" iImg="icon02.png" ftitle="Choose A Service"  descriptions="Why I say old chap that is spiffing bodge, blag pardon me buggered mufty Oxford butty bubble and squeak wind up, brown bread the full monty bloke ruddy cras tickety-boo squiffy. Starkers dropped a clanger lurgy is cack excuse my French what a plonker blower.!"/>
+                        <div className="agency_featured_content wow fadeInLeft" data-wow-delay="0.6s">
+                            <a href=""><button className="btn-circle icon mt_30 float-right"><i className="ti-arrow-right"></i></button></a> 
+                        </div>
+
+                        <Featuresitems rowClass="row flex-row-reverse" aClass="pr_70 pl_70" fimage="add-on.jpg" iImg="icon3.png" ftitle="Choose Additional Services"
                         descriptions="Why I say old chap that is spiffing bodge, blag pardon me buggered mufty Oxford butty bubble and squeak wind up, brown bread the full monty bloke ruddy cras tickety-boo squiffy. Starkers dropped a clanger lurgy is cack excuse my French what a plonker blower.!"/>
-                        <Featuresitems rowClass="row" aClass="pl_100" fimage="process_4.png" iImg="icon_04.png" ftitle="Confirm Booking" 
+                        <div className="agency_featured_content wow fadeInLeft" data-wow-delay="0.6s">
+                            <a href="/get-a-quote"><button className="btn-circle icon mt_30"><i className="ti-arrow-right"></i></button></a> 
+                        </div>
+                        
+                        <Featuresitems rowClass="row" aClass="pl_100" fimage="confirm-booking.jpg" iImg="icon_04.png" ftitle="Confirm Booking" 
                         descriptions="Why I say old chap that is spiffing bodge, blag pardon me buggered mufty Oxford butty bubble and squeak wind up, brown bread the full monty bloke ruddy cras tickety-boo squiffy. Starkers dropped a clanger lurgy is cack excuse my French what a plonker blower.!"/>
-                        <Featuresitems rowClass="row flex-row-reverse" aClass="pr_70 pl_70" fimage="process_5.png" iImg="icon_05.png" ftitle="Confirm Booking" 
+                        <div className="agency_featured_content wow fadeInLeft" data-wow-delay="0.6s">
+                            <a href="/cart"><button className="snipcart-checkout icon mt_30 float-right"><i className="ti-arrow-right"></i></button></a> 
+                        </div>
+
+                        <Featuresitems rowClass="row flex-row-reverse" aClass="pr_70 pl_70" fimage="relax.jpg" iImg="icon_05.png" ftitle="Choose Add-ons" 
                         descriptions="Why I say old chap that is spiffing bodge, blag pardon me buggered mufty Oxford butty bubble and squeak wind up, brown bread the full monty bloke ruddy cras tickety-boo squiffy. Starkers dropped a clanger lurgy is cack excuse my French what a plonker blower.!"/>
+                        <div className="agency_featured_content wow fadeInLeft" data-wow-delay="0.6s">
+                            <a href="/store"><button className="btn-circle icon mt_30"><i className="ti-arrow-right"></i></button></a>  
+                        </div>
+                        
                         <div className="dot middle_dot"><span className="dot1"></span><span className="dot2"></span></div>
                     </div>
-                </div>
+                </div> 
                                 
                            
             </section>
 
 <FreeEstimate />
             
-           
+<div className="dot middle_dot"><span className="dot1"></span><span className="dot2"></span></div>         
 </div>
 
 
 <Index3 />
-
+<div className="dot middle_dot"><span className="dot1"></span><span className="dot4"></span></div>
 
 <Index4 />
+<div className="dot middle_dot"><span className="dot3"></span><span className="dot4"></span></div>
 <Index5 />
+
 <Footer2 />
  
                 </>
